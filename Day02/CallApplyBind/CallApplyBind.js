26a1b91a56cabf1fba94eec3bb8cde7c8d12cb2f
Shivam Yadav <Shivamy2@mobiquityinc.com>
2023-06-12 18:34:32 +0530

    Added solution for exercise 4 (Call Apply and Binding)

---
+++ Day02/CallApplyBind/CallApplyBind.js
@@ -0,0 +1,38 @@
+function car(regNum, brand){
+    this.regNum = regNum;
+    this.brand = brand;
+}
+
+car.prototype.carDetails = function(){
+    return {
+        "Registration no" : this.regNum,
+        "Brand" : this.brand
+    }
+}
+
+function getOwnersVehicleDetail(owner,city){
+    console.log({...this.carDetails(),...owner,city});
+}
+
+function person(name,year){
+        this.name=name;
+        this.year=year;
+    }
+    
+    const shiv = new person("shivam","2022");
+    const aryn = new person("aryn","2001");
+    const jay = new person("jay","2013");
+    
+    const audi = new car("up32dn","audi");
+    const neo = new car("up34xs","tata");
+    const thar = new car("up36pq","mahindra");
+    //call example
+    getOwnersVehicleDetail.call(audi,shiv,"LKO")
+    //apply
+    getOwnersVehicleDetail.apply(neo,[aryn,"AMD"])
+    //bind
+    detailOfJaysVehicle=getOwnersVehicleDetail.bind(audi,jay,"CHN")
+
+    detailOfJaysVehicle();
+
+
